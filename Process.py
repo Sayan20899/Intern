127fb507873c95c79082255ab8aa93e6ffe279b1
Sayan20899 <122215565+Sayan20899@users.noreply.github.com>
2024-04-23 13:52:52 +0200

    Update Process.py

--- Process.py
+++ Process.py
@@ -1,32 +1,61 @@
-from CELL import Cell
-from FactoryPi import FactoryPi
+import json
 
+class Process:
+    def __init__(self):
+        self.mprocess = []
+        
 
-class process:
-    def __init__(self, fam):
-        self.fam = []
+    def add_cellid(self, cell_ids):
+        self.mprocess.append(cell_ids)
+
+    def represent(self):
+        data = []  # Initialize an empty dictionary to hold the JSON data
+        for i, family in enumerate(self.mprocess, start=1):  # Iterate over each family in self.family
+            #json_data.append({f"Family {i}":family})  # Add the family data to the dictionary
+            data.append(family)
+        self.mprocess = data
+        
+        return data  # Return the JSON data as a dictionary
+
     
-    def add(self, fac: FactoryPi):
-        self.fam.append(fac.family)
-            
 
-def getstring(factorypi_obj: FactoryPi):
-    """
-    Creates cells (if not already created), adds cell IDs to the 
-    family list of the provided FactoryPi object, and prints the family.
-    """
-    if not factorypi_obj.cells:
-        factorypi_obj.cells = factorypi_obj.create_cells_and_resources()  # Create cells if needed
+# Example usage
+if __name__ == "__main__":
+    process = Process()
+    process.add_cellid([1, 2, 3]) # Family 1
+    process.add_cellid([1, 2, 3])
+    process.add_cellid([1, 2, 3])
+    process.add_cellid([1, 2, 3])
+    process.add_cellid([1, 2, 3])
+    #process.add_family([2, 3])      # Family 2
 
-    for cell in factorypi_obj.cells:
-        factorypi_obj.addFamily(cell.cell_id)  # Access cell_id directly
-        print(f"Family: {factorypi_obj.family}")  # Use f-string for cleaner formatting
+    # Convert to JSON
+    json_string = process.represent()
+    manufacturing_process = {}
+    for i, family_data in enumerate(json_string, start=1):
+        manufacturing_process[f"Family{i}"] = family_data
 
-# Create an instance of FactoryPi
-factorypi = FactoryPi("F_5F3C6M", cells , family= [])
+    result = {
+    "Manufacturing Process": manufacturing_process
+    }
+    json_output = json.dumps(result, indent='\n')
+    
+    with open('process.json', 'w') as outfile:
+        json.dump(result, outfile, indent=4)
+        
+        
+    print(json_output)
+    #print(process.mprocess)
+    #print(result)
+    #print(json_string)
+    # Reconstruct object from JSON
+    #reconstructed_process = ManufacturingProcess.from_json(json_data)
+    #print("\nReconstructed Manufacturing Process:")
+    #for i, family in enumerate(reconstructed_process.family, start=1):
+        #print(f"Family {i}:", family)
 
-# Call getstring with the FactoryPi object
-getstring(factorypi)
+
+
     
     
     
